ff1fc63143ab1519acefc8be6089e5c771dec6fa
Simon Ayala <simonlisandroayala@gmail.com>
2018-01-04 21:48:20 -0300

    Add build_word_counts which builds words->(doc, freq) dictionary

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,7 @@
 """LA Times Articles Preprocessing"""
 import sys
+from sklearn.feature_extraction.text import CountVectorizer
+from itertools import groupby
 
 def parse_xml(xmlfile):
     """Parse xml and return a list of documents as strings"""
@@ -15,12 +17,28 @@ def parse_xml(xmlfile):
                 new_doc += line
 
 
+def build_word_counts(docs):
+    # Initialize an fit CountVectorizer
+    cv = CountVectorizer()
+    X = cv.fit_transform(docs).T.tocoo() # Transpose matrix and set appropiate format
+    # Get vocabulary
+    words = cv.get_feature_names()
+    # Build finall structure
+    result = {}
+    sorted_occurrences = sorted(zip(X.row, X.col, X.data), key=lambda x: x[0])
+    groups = groupby(sorted_occurrences, key=lambda x: x[0])
+    for w_id, w_list in groups:
+        w_list = [(x[1], x[2]) for x in w_list]
+        result[words[w_id]] = list(w_list)
+    return result
+
+
 def main():
     """Main"""
     path = sys.argv[1]
     with open(path) as (xmlfile):
         docs = parse_xml(xmlfile)
-
+        build_word_counts(docs)
 
 if __name__ == '__main__':
     main()
